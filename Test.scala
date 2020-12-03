09a5d5c584f23f4518a55856e5b6ecaddaef49f3
Tobias Schlatter <tobias@meisch.ch>
2020-12-03 09:51:39 +0100

    Enable verbose logging

--- Test.scala
+++ Test.scala
@@ -2,6 +2,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 object Test {
   def main(args: Array[String]): Unit = {
+    System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+    System.setProperty("webdriver.chrome.verboseLogging", "true");
+
     val driver = new ChromeDriver()
     try {
       val data = driver.executeScript("return \"\u007F\";").asInstanceOf[String]
